f0cee737983c917409c7e05d5f6ad977f9f09344
gamejoye <gamejoye@gmail.com>
2022-07-21 03:44:35 +0800

    重构了博客(之前的部分功能还未补全)

--- src/pages/Titles/index.js
+++ src/pages/Titles/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { useEffect } from "react";
 
 import { getInstance } from "../../utils/apis/axiosConfig";
 import TitleList from "./TitleList";
@@ -9,15 +10,17 @@ import { getCookie } from "../../utils/apis/getCookie";
 function Titles(props) {
     const [titles,setTitles] = useState([]);
     const username = getCookie("username");
-    getInstance.get(baseUrl + 'titles/byName', {
-        params: {
-            username: username
-        }
-    }).then(
-        (res) => {
-            setTitles(res.data);
-        }
-    );
+    useEffect(() => {
+        getInstance.get(baseUrl + 'titles/byName', {
+            params: {
+                username: username
+            }
+        }).then(
+            (res) => {
+                setTitles(res.data);
+            }
+        );
+    },titles);
 
     return (
         <TitleList titles={titles}></TitleList>
