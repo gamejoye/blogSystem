98273ea5b4a10e15601725d34610505298539790
gamejoye <gamejoye@gmail.com>
2022-07-31 01:31:50 +0800

    添加了一点样式

--- src/pages/About/AboutMe/index.js
+++ src/pages/About/AboutMe/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Radio, DatePicker } from "antd";
+import { Radio, DatePicker } from "antd";
 import moment from "moment";
 import './index.css'
 
@@ -14,49 +14,64 @@ function AboutMe(props) {
             <div>
                 <p>个人简介:</p>
                 {(isEdit & (1 << 0) &&
-                    <div>
+                    <div className="para">
                         <textarea
                             className="aboutMe"
                             defaultValue={preState["aboutMe"]}
                             onChange={(e) => props.setAboutMe(e.target.value)}
                         /><br />
-                        <Button onClick={() => props.handlerSubmit("aboutMe", 0)}>Submit</Button>
-                        <Button onClick={() => props.handlerCancel("aboutMe", 0)}>Cancel</Button>
+                        <button onClick={() => props.handlerSubmit("aboutMe", 0)}>Submit</button>
+                        <button onClick={() => props.handlerCancel("aboutMe", 0)}>Cancel</button>
                     </div>) ||
-                    <div>
-                        <p>{props.preState["aboutMe"]}</p>
-                        <Button onClick={() => props.setEdit(isEdit ^ (1 << 0))}>Edit</Button>
+                    <div className="para">
+                        <span>
+                            <p>{props.preState["aboutMe"]}</p>
+                            <button
+                                id="aboutMeEdit"
+                                onClick={() => props.setEdit(isEdit ^ (1 << 0))}
+                            >Edit</button>
+                        </span>
                     </div>
                 }
                 <p>性别:</p>
                 {(isEdit & (1 << 1) &&
-                    <div>
+                    <div className="para">
                         <Radio.Group onChange={(e) => (props.setSex(e.target.value))} defaultValue={preState["sex"]}>
                             <Radio value={'男'}>男</Radio>
                             <Radio value={'女'}>女</Radio>
                         </Radio.Group>
-                        <Button onClick={() => props.handlerSubmit("sex", 1)}>Submit</Button>
-                        <Button onClick={() => props.handlerCancel("sex", 1)}>Cancel</Button>
+                        <button onClick={() => props.handlerSubmit("sex", 1)}>Submit</button>
+                        <button onClick={() => props.handlerCancel("sex", 1)}>Cancel</button>
                     </div>) ||
-                    <div>
-                        <p>{props.preState["sex"]}</p>
-                        <Button onClick={() => props.setEdit(isEdit ^ (1 << 1))}>Edit</Button>
+                    <div className="para">
+                        <span>
+                            <p>{props.preState["sex"]}</p>
+                            <button
+                                id="sexEdit"
+                                onClick={() => props.setEdit(isEdit ^ (1 << 1))}
+                            >Edit</button>
+                        </span>
                     </div>
                 }
                 <p>生日:</p>
                 {(isEdit & (1 << 2)) &&
-                    <div>
+                    <div className="para">
                         <DatePicker className="calendar" placeholder="请设置您的出生日期"
                             onChange={(e) => props.setBirthday(moment(e._d).format(dateForm))}
                             showToday={true}
-                            //onChange={(e) => console.log(moment(e._d).format('YYYY-MM-DD'))}
+                        //onChange={(e) => console.log(moment(e._d).format('YYYY-MM-DD'))}
                         />
-                        <Button onClick={() => props.handlerSubmit("birthday", 2)}>Submit</Button>
-                        <Button onClick={() => props.handlerCancel("birthday", 2)}>Cancel</Button>
+                        <button onClick={() => props.handlerSubmit("birthday", 2)}>Submit</button>
+                        <button onClick={() => props.handlerCancel("birthday", 2)}>Cancel</button>
                     </div> ||
-                    <div>
-                        <p>{props.preState["birthday"]}</p>
-                        <Button onClick={() => props.setEdit(isEdit ^ (1 << 2))}>Edit</Button>
+                    <div className="para">
+                        <span>
+                            <p>{props.preState["birthday"]}</p>
+                            <button
+                                id="birthdayEdit"
+                                onClick={() => props.setEdit(isEdit ^ (1 << 2))}
+                            >Edit</button>
+                        </span>
                     </div>
                 }
             </div>
