c53827bb75fcb4fca31f4e11aa96259bab7f8c18
gamejoye <gamejoye@gmail.com>
2022-08-12 14:37:24 +0800

    creation pages 样式

--- src/pages/Creation/index.js
+++ src/pages/Creation/index.js
@@ -5,6 +5,9 @@ import { useNavigate } from "react-router";
 
 import { postInstance } from "../../utils/apis/axiosConfig";
 import { username } from "../../constant";
+import TextArea from "antd/lib/input/TextArea";
+import { Button, Col, Divider, Row } from "antd";
+import './index.css'
 
 function Creation(props) {
     const navigate = useNavigate();
@@ -13,14 +16,14 @@ function Creation(props) {
     const [order, setOrder] = useState(1);
 
     function handlerSubmit() {
-        postInstance.post('blogs/'+'addition',{
+        postInstance.post('blogs/' + 'addition', {
             username: username,
             title: title,
             content: content,
             order: order
         }).then(
             (res) => {
-                if(res.data === 'successfully added') {
+                if (res.data === 'successfully added') {
                     navigate('/home');
                 } else {
                     alert('This blog already exists, please change it to a new blog name');
@@ -30,23 +33,29 @@ function Creation(props) {
     }
     return (
         <div>
-            <b>title:</b><input
-                id="titleInput"
-                onChange={(e) => { setTitle(e.target.value) }}
-            /><br/>
-            <b>content:</b><textarea rows="10" cols="30"
-                id="contentInput"
-                onChange={(e) => { setContent(e.target.value) }}
-            >
-            </textarea><br/>
-            <b>order:</b><input
-                id="orderInput"
-                type="number"
-                onChange={(e) => { setOrder(e.target.value) }}
-            /><br/>
-            <button
-                onClick={handlerSubmit}
-            >Submit</button>
+            <Row gutter={[16, 16]}>
+                <Col span={14} offset={4}>
+                    <br />
+                    <TextArea
+                        className="head"
+                        rows={1}
+                        placeholder="文章标题~"
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </Col>
+                <Col span={14} offset={4}>
+                    <br />
+                    <TextArea
+                        className="text"
+                        rows={35}
+                        placeholder="文章内容~"
+                        onChange={(e) => setContent(e.target.value)}
+                    />
+                </Col>
+                <Col span={4} offset={10}>
+                    <Button type="primary" onClick={() => handlerSubmit()}>发布博客</Button>
+                </Col>
+            </Row>
         </div>
     )
 }
