9014f5c0ff18da239d1e696363182a75417a6104
gamejoye <gamejoye@gmail.com>
2022-11-30 14:54:41 +0800

    小重构

---
+++ src/redux/actions/nameAction.js
@@ -0,0 +1,5 @@
+import { SET_NAME } from "../constant";
+export const setName = data => ({
+    type: SET_NAME,
+    data
+});
---
+++ src/redux/actions/navAction.js
@@ -0,0 +1,6 @@
+import { SET_NAV_SHOW } from "../constant";
+
+export const setNav = data => ({
+    type: SET_NAV_SHOW,
+    data
+});
